47468c2f191bb03ee3d3b71ac9173594341bccb6
Aapo Vienmao <aapo.vienamo@aalto.fi>
2019-11-06 16:39:42 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import subprocess
+
+try:
+    systemd_service_path = subprocess.check_output(
+        ['pkg-config', '--variable=systemdsystemunitdir', 'systemd']
+    ).decode('utf-8').rstrip()
+    systemd_service_data_file = [(systemd_service_path, ['botani.service'])]
+except:
+    print("NOTE: Could not determine systemd service file install path.")
+    print("      Possibly either pkg-config or systemd is missing.")
+    systemd_service_data_file = []
+
+setup(
+    name='botani',
+    version='0',
+    packages=['botani'],
+    license='MIT',
+    author="Aapo Vienamo & Juuso Laine",
+    author_email="aapo.vienamo@aalto.fi",
+    long_description='Python script for measuring and sending plant moisture data to influxdb',
+    entry_points = {
+        'console_scripts': [
+            'botani=botani.botani:main',
+        ]
+    },
+    data_files = [
+        ('/etc/', ['botani.conf']),
+    ] + systemd_service_data_file,
+    install_requires=['RPi.GPIO', 'influxdb'],
+)

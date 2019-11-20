da65bb7dd5c4c73ecea4043f6c0f7fa8df06aa08
Aapo Vienmao <aapo.vienamo@aalto.fi>
2019-11-20 16:30:51 +0200

    Add a service file for enabling adc

--- install-extras.sh
+++ install-extras.sh
@@ -2,5 +2,6 @@
 
 SYSTEMD_UNIT_DIR=$(pkg-config --variable=systemdsystemunitdir systemd)
 install -D botani.service $SYSTEMD_UNIT_DIR
+install -D ads1115-i2c.service $SYSTEMD_UNIT_DIR
 install -D botani-influxdb.conf /etc/botani/influxdb.conf
 install -D botani-plants.conf /etc/botani/plants.conf
